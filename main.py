cb76cb4f58742077c6318b16048505562e61b16a
李東霖 <lee097019@gmail.com>
2018-04-10 14:38:07 +0800

    Improve performance

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#import jieba
 import argparse
-import pandas as pd
-import json
-import codecs
 import re
+import pandas as pd
 import time
 
 def parseQuery(query):
+    if query[-1] == '\n':
+        query = query[:-1]
     words = query.split(" ")
     op = None
     queryWord = []
@@ -15,8 +14,13 @@ def parseQuery(query):
             op = word
             continue
         queryWord.append(word)
-    
     return [queryWord, op]
+
+def splitKeyWordByRE(str,n):
+    words = [re.findall(r"[\u4e00-\ufaff]{1}|[0-9]+|[a-zA-Z]+\'*[a-zA-Z]*", str, re.UNICODE)]        
+    for i in range(2,n+1):
+        words.append(re.findall(r"(?=([\u4e00-\ufaff]{%d}))" % (i), str, re.UNICODE))
+    return words
 
 def English(str):
     regex = r"[a-zA-Z]+\'*[a-z]*"
@@ -26,12 +30,37 @@ def English(str):
     else:
         return False
 
-def spliteKeyWord(str):
-    regex = r"[\u4e00-\ufaff]|[0-9]+|[a-zA-Z]+\'*[a-z]*|[^0-9]"
-    matches = re.findall(regex, str, re.UNICODE)
-    return matches
+class searchEngine:
+    def __init__(self, n):
+        self.dicts = []
+        self.dictAll = dict()
+        for i in range(n):
+            self.dicts.append(dict())
+    
+    def put(self, n, word, index):
+        if n >= len(self.dicts):
+            return
+        if word not in self.dicts[n]:
+            self.dicts[n][word] = set([index])
+        else:
+            self.dicts[n][word].add(index)
+    
+    def putAll(self, word, index):
+        if word not in self.dictAll:
+            self.dictAll[word] = set([index])
+        else:
+            self.dictAll[word].add(index)
+    
+    def query(self, word):
+        if English(word):
+            return self.dicts[0].get(word, set())
+        else:
+            return self.dicts[len(word) - 1].get(word, set())
+    
+    def queryAll(self, word):
+        return self.dictAll.get(word, set())
 
-
+n_words = 3
 
 if __name__ == '__main__':
     # You should not modify this part.
@@ -47,89 +76,49 @@ if __name__ == '__main__':
                         default='output.txt',
                         help='output file name')
     args = parser.parse_args()
-    
-    # Please implement your algorithm below
-    
-    # init
-    #jieba.set_dictionary('./dict.txt.big.txt')
-    #jieba.initialize()
 
     # TODO load source data, build search engine
     source = pd.read_csv(args.source, header=None)
-    source = source.iloc[:,1].values
-
-    start_time = time.time()
-    engine = [dict(), dict(), dict()]
-    for index,title in enumerate(source):
-        #print(row[0])
-        #title = row[1]
-        #terms = jieba.cut(title)
-        #terms = jieba.cut_for_search(title)
-        
-
-        #terms = list()
-        words = spliteKeyWord(title)
-
-        for j in range(1,4):
-            for i in range(0,len(words) - j + 1):
-                term = ''.join(words[i:i+j])
-                if term not in engine[j-1]:
-                    engine[j-1][term] = set()
-                engine[j-1][term].add(index+1)
-
     
-    print("build engine finish for %s sec" % (time.time() - start_time))
-    
-    #with open('dictlist.txt','w') as dictfile:
-    #    for i in range(0,3):
-    #        for key in engine[i]:
-    #            dictfile.write(key)
-    #            dictfile.write('\n')
-    
+    sourceData = source.iloc[:,1].values
+
+    engine = searchEngine(n_words)
+    for index, title in enumerate(sourceData):
+        terms = splitKeyWordByRE(title,n_words)
+        for i in range(n_words):
+            for term in terms[i]:
+                engine.putAll(term, index+1)
     
     # TODO compute query result
-    
+    with open(args.query, 'r') as queryfile:
+        querys = queryfile.readlines()
 
-  
-    # TODO output result
-    start_time = time.time()
-    ouputfile = open(args.output, "w")
-    querylist = pd.read_csv(args.query, header=None)
+    answer1 = []
 
-    for index,row in querylist.iterrows():
-        querys, op = parseQuery(row[0])
-        #print('query : ', querys)
-        #print('op : ', op)
-        answer = None
-        for i in querys:
-            if English(i):
-                engine_index = 0
-            else:
-                engine_index = len(i) - 1
+    for query in querys:
+        keywords, op = parseQuery(query)
 
-            if answer == None:
-                answer = engine[engine_index].get(i, set())
-            else:
-                if op == "and":
-                    answer = answer & engine[engine_index].get(i, set())
-                if op == "or":
-                    answer = answer | engine[engine_index].get(i, set())
-                if op == "not":
-                    answer = answer - engine[engine_index].get(i, set())
-        
-        #print(answer)
+        answer = engine.queryAll(keywords[0])
+        for keyword in keywords[1:]:
+            if op == "and":
+                answer = answer & engine.queryAll(keyword)
+            if op == "or":
+                answer = answer | engine.queryAll(keyword)
+            if op == "not":
+                answer = answer - engine.queryAll(keyword)
+
         answer = list(answer)
         answer.sort()
-        #print(answer)
         if len(answer) == 0:
-            answer = "0"
+            answer = '0'
         else:
-            answer = ",".join(str(num) for num in answer)
+            answer = ','.join(str(num) for num in answer)
 
-        if index != 0:
-            ouputfile.write('\n')
-        ouputfile.write(answer)
-    
-    ouputfile.close()
-
-    print("query finish for %s sec" % (time.time() - start_time))
+        answer1.append(answer)
+  
+    # TODO output result
+    with open(args.output, "w") as ouputfile:
+        for i in range(len(answer1)):
+            if i!=0:
+                ouputfile.write('\n')
+            ouputfile.write(answer1[i])
